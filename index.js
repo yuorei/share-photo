23385e40664ea2c2dd533ffdd79427f9589b4d2e
yuorei <yuorei71@gmail.com>
2023-02-17 12:50:43 +0900

    タグつけ

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ const typeDefs = `
         name: String!
         avatar: String
         postedPhotos: [Photo!]!
+        inPhotos: [Photo!]!
     }
 
     type Photo {
@@ -26,6 +27,7 @@ const typeDefs = `
         description: String
         category: PhotoCategory!
         postedBy: User!
+        taggedUsers: [User!]!
     }
 
     input PostPhotoInput {
@@ -44,14 +46,46 @@ const typeDefs = `
     }
 `
 var _id = 0
+var users = [
+    { "githubLogin": "yuorei", "name": "ユオレイ" },
+    { "githubLogin": "sSchmidt", "name": "スミスさん" }
+]
 // 写真を格納するための配列
-var phots = []
+var photos = [
+    {
+        "id": "1",
+        "name": "すごい車",
+        "description": "はやい",
+        "category": "ACTION",
+        "githubUser": "yuorei"
+    },
+    {
+        "id": "2",
+        "name": "すごい家",
+        "category": "SELFIE",
+        "githubUser": "sSchmidt"
+    },
+    {
+        "id": "3",
+        "name": "すごい子",
+        "description": "えらい",
+        "category": "LANDSCAPE",
+        "githubUser": "yuorei"
+    },
+]
+
+var tags = [
+    {"photoID": "1","userID": "yuorei"},
+    {"photoID": "2","userID": "yuorei"},
+    {"photoID": "2","userID": "sSchmidt"}
+]
+
 
 // リゾルバは特定のフィールドのデータを返す関数
 const resolvers = {
     Query: {
-        totalPhotos: () => phots.length,
-        allPhotos: () => phots
+        totalPhotos: () => photos.length,
+        allPhotos: () => photos
     },
 
     Mutation: {
@@ -63,7 +97,7 @@ const resolvers = {
                 id: _id++,
                 ...args.input
             }
-            phots.push(newPhoto) //本来はここでDBに入れる
+            photos.push(newPhoto) //本来はここでDBに入れる
 
             // 新しい写真を返す
             return newPhoto
@@ -73,13 +107,33 @@ const resolvers = {
         // urlの生成
         url: parent => `http://自分のサイト/img/${parent.id}.jpg`,
         postedBy: parent => {
-            return users.find(u => u.githubLogin === parent.githubLogin)
-        }
+            return users.find(u => u.githubLogin === parent.githubUser)
+        },
+        taggedUsers: parent => tags
+
+            // 対象の写真が関係しているタグの配列を返す
+            .filter(tags => tags.photoID === parent.id)
+
+            // タグの配列をユーザーIDの配列の変換する
+            .map(tag => tag.userID)
+
+            // ユーザーIDの配列をユーザーオブジェクトの配列に変換する
+            .map(userID => users.find(u => u.githubLogin === userID))
     },
     User: {
         postedPhotos: parent => {
-            return phots.filter(p => p.githubUser === parent.githubLogin)
-        }
+            return photos.filter(p => p.githubUser === parent.githubLogin)
+        },
+        inPhotos: parent => tags
+
+            // 対象のユーザーが関係しているタグの配列を返す
+            .filter(tags => tags.userID === parent.id)
+
+            // タグの配列を写真IDの配列の変換する
+            .map(tag => tag.photoID)
+
+            // 写真IDの配列を写真オブジェクトの配列に変換する
+            .map(photoID => photos.find(p => p.id === photoID))
     }
 }
 const server = new ApolloServer({
